90429b797d396dd050fc6bda121e4c9da972f02c
khuxkm fbexl <khuxkm@ttm.sh>
2018-11-18 07:39:46 +0000

    Update gitea handlers

--- handlers/gitea.js
+++ handlers/gitea.js
@@ -6,6 +6,10 @@ exports.use = function(conf) {
 	colors = conf.colors;
 }
 
-exports.push = function(bot,body) {
-	bot.say(config.channel,colors.wrap("dark_blue",body.pusher.full_name)+" pushed "+colors.codes.gray+body.commits.length+" commit"+(body.commits.length!=1 ? "s" : "")+colors.codes.reset+(body.ref.startsWith("refs/heads/") ? " to "+colors.wrap("magenta",body.ref.replace("refs/heads/","")) : "")+". ("+(body.commits.map(x => x.message.split('\n')[0]).join(", "))+")");
+function getPrefix(body) {
+	return colors.wrap("light_blue","[")+colors.wrap("light_gray",body.repository.full_name)+colors.wrap("light_blue","]")+" ";
+}
+
+exports.push = function(channel,bot,body) {
+	bot.say(channel,getPrefix(body)+colors.wrap("dark_blue",body.pusher.full_name)+" pushed "+colors.codes.gray+body.commits.length+" commit"+(body.commits.length!=1 ? "s" : "")+colors.codes.reset+(body.ref.startsWith("refs/heads/") ? " to "+colors.wrap("magenta",body.ref.replace("refs/heads/","")) : "")+". ("+(body.commits.map(x => x.message.split('\n')[0]).join(", "))+")");
 }
