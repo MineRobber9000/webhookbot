17278b8c96964357b396b79537dc5cace6b4a7d3
khuxkm fbexl <khuxkm@ttm.sh>
2018-11-18 05:32:36 +0000

    Add more logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var log = require("./log").log;
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync("config.json"));
+log(config.toString());
 const util = require("./util");
 const irc = require("irc-upd");
 util.use(config);
@@ -7,14 +9,15 @@ var opts = {channels:[config.channel]};
 opts = Object.assign(opts,config.opts);
 var bot = new irc.Client(config.server,config.nick,opts);
 var app = require("express")();
-app.use(require("body-parser").json({verify:function(req,res,buf,encoding){ req.rawData = buf.toString(encoding); }}));
+app.use(require("body-parser").json({verify:function(req,res,buf,encoding){ req.rawData = buf.toString(encoding); log(req.rawData); }}));
 app.post("/webhook/",function(req,res) {
+	log("Request hit the endpoint!");
 	if (util.matchSecret(req,config.secret)) {
 		util.on(req.get("X-GitHub-Event"),req.body,bot);
 	}
 	res.status(200).end();
 });
-app.listen(8001,function() {console.log("running on port 8001");});
+app.listen(8001,function() {log("HTTP server running on port 8001");});
 bot.addListener('error', function(message) {
-	console.log('error: ', message);
+	log('error: ', message);
 });
